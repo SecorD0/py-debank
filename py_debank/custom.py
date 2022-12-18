15e3e519fae81048d1a6703de025487a8ee7fc63
SecorD0 <25284138+SecorD0@users.noreply.github.com>
2022-12-18 19:20:01 +0300

    Add the 'parse_nfts' argument to the 'get_balance' function

--- py_debank/custom.py
+++ py_debank/custom.py
@@ -6,7 +6,7 @@ from py_debank import token
 from py_debank.models import Chain, ChainNames
 
 
-def get_balance(address: str, chain: ChainNames or str = '',
+def get_balance(address: str, chain: ChainNames or str = '', parse_nfts: bool = True,
                 proxies: Optional[str or List[str]] = None) -> Dict[str, Chain]:
     """
     Get the following information of an address of one or all chains:
@@ -16,33 +16,36 @@ def get_balance(address: str, chain: ChainNames or str = '',
 
     :param str address: the address
     :param ChainNames or st chain: the chain (all chains)
+    :param bool parse_nfts: whether to parse NFT, it leads to a high probability of "429 Too Many Requests" error (True)
     :param Optional[str or List[str]] proxies: an HTTP proxy or a proxy list for random choice for making a request (None)
     :return Chain: the address information
     """
     chains: Dict[str, Chain] = {}
     if chain:
-        nfts = nft.collection_list(address=address, chain=chain, proxies=proxies)
-        chains.update(nfts)
+        tokens = token.balance_list(address=address, chain=chain, proxies=proxies)
+        chains.update({chain: tokens})
 
-        tokens = token.balance_list(address=address, chain=chain, raw_data=True, proxies=proxies)
-        if tokens:
-            chains[chain].parse_tokens(tokens[chain])
+        if parse_nfts:
+            nfts = nft.collection_list(address=address, chain=chain, raw_data=True, proxies=proxies)
+            if nfts:
+                chains[chain].parse_nfts(nfts[chain])
 
         projects = portfolio.project_list(address=address, raw_data=True, proxies=proxies)
         if chain in projects:
             chains[chain].parse_projects(projects[chain])
 
     else:
-        nfts = nft.collection_list(address=address, proxies=proxies)
-        chains.update(nfts)
+        tokens = token.cache_balance_list(address=address, proxies=proxies)
+        chains.update(tokens)
 
-        tokens = token.cache_balance_list(address=address, raw_data=True, proxies=proxies)
-        for name, token_dict in tokens.items():
-            if name in chains:
-                chains[name].parse_tokens(token_dict)
+        if parse_nfts:
+            nfts = nft.collection_list(address=address, raw_data=True, proxies=proxies)
+            for name, nft_dict in nfts.items():
+                if name in chains:
+                    chains[name].parse_nfts(nft_dict)
 
-            else:
-                chains[name] = Chain(name=name, tokens=token_dict)
+                else:
+                    chains[name] = Chain(name=name, tokens=nft_dict)
 
         projects = portfolio.project_list(address=address, raw_data=True, proxies=proxies)
         for name, project_dict in projects.items():
